9604e1dc9cbf7209756093a409b1a8f830f360a7
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-11-22 12:06:01 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,37 @@
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+import serial
+import time
+import subprocess
+import decimal
+
+class swordSkill(MycroftSkill):
+
+    def __init__(self):
+        super().__init__()
+        self.learning = True
+
+    def initialize(self):
+        my_setting = self.settings.get('my_setting')
+
+    @intent_handler('princess.intent')
+    def handle_not_are_you_intent(self, message):
+        self.speak_dialog("This is not just for decoration")
+        serA = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        serA.flush()
+        serA.write(b"sword")
+        serB = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+        serB.flush()
+        serB.write(b"sword")        
+        serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
+        serC.flush()
+        serC.write(b"sword")
+        serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        serD.flush()
+        serD.write(b"sword")
+
+    def stop(self):
+        pass
+
+def create_skill():
+    return swordSkill()
