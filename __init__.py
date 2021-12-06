1571ee3e67461381daeb5e24862e87e05e6e80ad
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-12-06 15:26:52 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,17 +18,21 @@ class swordSkill(MycroftSkill):
     def handle_not_are_you_intent(self, message):
         self.speak_dialog("This is not just for decoration")
         serA = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        serA.flush()
-        serA.write(b"sword")
         serB = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
-        serB.flush()
-        serB.write(b"sword")        
         serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
+        serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        time.sleep(0.5)
+        serA.flush()
+        serB.flush()
         serC.flush()
+        serD.flush()
+        time.sleep(0.5)
+        serA.write(b"sword")
+        serB.write(b"sword")        
         serC.write(b"sword")
-        #serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
-        #serD.flush()
-        #serD.write(b"sword")
+        serD.write(b"sword")
+        time.sleep(0.5)
+        self.speak_dialog("This is not just for decoration")
 
     def stop(self):
         pass
